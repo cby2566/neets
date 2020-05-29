fe0618fca9d69b59b529bb177c61dd00f771b6df
cby2566 <870017005@qq.com>
2020-05-29 22:48:18 +0800

    fix:no state,class component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import LsComp from './components/listComponents.js';
+
+
+
 function App() {
+  let x = 12;
+  let user = {
+    name: '沙赞',
+    age: 14
+  }
+  function upname2(){
+    user.name = '贝尔'
+    console.log(user.name)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HelloMessage names={user.name}/>
+        <span>{x}</span>
+        <input type="button" value="update!" onClick={upname2}></input>
+        <LsComp userInfo={user} />
       </header>
     </div>
   );
 }
-
+function HelloMessage(props) {
+  return <h1>Hello World! {props.names}</h1>;
+}
 export default App;
---
+++ src/components/listComponents.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+class LsComp extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+      }
+
+    upName(){
+        this.setState({
+            date: "雷霆沙赞"
+          });
+        console.log(this.state.date)
+    }
+    componentWillMount(){
+        setInterval(()=>{
+            this.setState({
+                date: new Date()
+              });
+        },1000)
+    }
+    render(){
+        return (
+            <div>
+                <h1>oooooook!</h1>
+                <h2 onClick={this.upName.bind(this)}>{ this.props.userInfo.name}</h2>
+                <h2>现在是 {this.state.date.toLocaleTimeString()}.</h2>
+                <HelloMessage names={this.state.date}/>
+            </div>
+        )
+    }
+}
+export default LsComp;
+
+
+function HelloMessage(props) {
+    return <h1>Hello 222</h1>;
+}
